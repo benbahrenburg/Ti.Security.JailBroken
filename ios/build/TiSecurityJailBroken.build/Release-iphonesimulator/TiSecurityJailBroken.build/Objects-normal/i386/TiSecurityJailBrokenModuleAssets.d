3ee384696b907b4cabb82ed2efe57262d563d67b
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2018-11-18 14:05:32 -0800

    Remove build contents

--- ios/build/TiSecurityJailBroken.build/Release-iphonesimulator/TiSecurityJailBroken.build/Objects-normal/i386/TiSecurityJailBrokenModuleAssets.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/bencoding/Projects/AppCWorkspace/TiSecurityJailBroken/ios/TiSecurityJailBroken_Prefix.pch \
-  /Users/bencoding/Projects/AppCWorkspace/TiSecurityJailBroken/ios/Classes/TiSecurityJailBrokenModuleAssets.m \
-  /Users/bencoding/Projects/AppCWorkspace/TiSecurityJailBroken/ios/Classes/TiSecurityJailBrokenModuleAssets.h
